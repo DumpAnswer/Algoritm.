44ccd2ae5cf04bd00659dd67e3c2307309123d36
DumpAnswer <kaleksers93@gmail.com>
2021-04-29 22:12:42 +0300

    Дописал тест

--- DZ4/BinaryTreeSearch/BinaryTreeSearch/Program.cs
+++ DZ4/BinaryTreeSearch/BinaryTreeSearch/Program.cs
@@ -6,16 +6,6 @@ namespace BinaryTreeSearch
     {
         static void Main(string[] args)
         {
-            TreeList tree = new TreeList();
-            tree.AddItem(33);
-            tree.AddItem(40);
-            tree.AddItem(20);
-            tree.AddItem(15);
-            tree.AddItem(17);
-            tree.AddItem(50);
-            tree.AddItem(60);
-
-            tree.PrintTree();
 
         }
     }
--- DZ4/BinaryTreeSearch/Test BinarTree/UnitTest1.cs
+++ DZ4/BinaryTreeSearch/Test BinarTree/UnitTest1.cs
@@ -9,32 +9,76 @@ namespace Test_BinarTree
         TreeList tree;
         
         [SetUp]
-        public void Setup()
+        public void Setup() // Добавление элементов 
         {
             tree = new TreeList();
+            tree.AddItem(33);
+            tree.AddItem(20);
+            tree.AddItem(25);
+            tree.AddItem(15);
+            tree.AddItem(40);
+            tree.AddItem(35);
+            tree.AddItem(45);
+
         }
 
         [Test]
-        public void AddItem_Test1() // тест на добавление в пустой список
+        public void GetNodeItem_Test1() // тест на поиск в левое части звена
 
         {
-            tree.AddItem(33);
+            tree.GetNodeByValue(20);
 
 
-            int actual = tree._root.Value;
-            int expected = 33;
+            int actual = tree._root.LeftChild.Value;
+            int expected = 20;
 
             Assert.AreEqual(expected, actual);
         }
-        public void AddItem_Test2() //тест на добавлнеи правого и левого элемента
+        [Test]
+        public void GetNodeItem_Test2() //тест на поиск правого звена 
         {
-            tree.AddItem(40);
-            tree.AddItem(20);
+            tree.GetNodeByValue(40);
+            
 
             int actual1 = tree._root.RightChild.Value;
-            int actual2 = tree._root.LeftChild.Value;
+            
 
             int expected1 = 40;
+            
+
+            Assert.AreEqual(expected1, actual1);
+           
         } 
+        [Test]
+        public void RemoveItem_Test1() //тест на удаление корня
+        {
+            tree.RemoveItem(33);
+
+            int actual = tree._root.Value;
+            int expected = 25;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void RemoveItem_Test2()//тест  на удаление узла имеющий и правый и левый узел
+        {
+            tree.RemoveItem(20);
+
+            int actual = tree._root.LeftChild.Value;
+            int expected = 25;
+
+            Assert.AreEqual(expected, actual);
+        }
+       
+        [Test]
+        public void RemoveItem_Testу3()// тест на удаление узла не имещий узлов
+        {
+            tree.RemoveItem(25);
+
+            var actual = tree._root.LeftChild.RightChild;
+            string expected = null;
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
